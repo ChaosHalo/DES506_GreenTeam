cc2cacea4a401b74b646b15422210fe628e9cc85
SerOleg <58004206+SerOleg@users.noreply.github.com>
2023-07-13 23:33:49 +0100

    Fixes
    - fix water shader
    - refactor perlin noise code

--- Assets/Scripts/TrackEditor/PerlinNoise.cs
+++ Assets/Scripts/TrackEditor/PerlinNoise.cs
@@ -5,13 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class PerlinNoise : MonoBehaviour
 {
-    public enum Type { PRESET_1, PRESET_2, PRESET_3 };
-    public Type generationType;
-
     private int heightPower = 2;
     private int heightOctave = 2;
-    private int desertPower = 1;
-    private int desertOctave = 2;
     private int waterPower = 2;
     private int waterOctave = 2;
 
@@ -19,13 +14,11 @@ public class PerlinNoise : MonoBehaviour
     internal const int sizeY = 10;
 
     float[,] heightPerlinNoise = new float[sizeX, sizeY];
-    float[,] desertPerlinNoise = new float[sizeX, sizeY];
     float[,] waterPerlinNoise = new float[sizeX, sizeY];
 
     public void BeginGenerate()
     {
         heightPerlinNoise = GeneratePerlinNoise(heightPower, heightOctave, GenerateBaseNoise());
-        desertPerlinNoise = GeneratePerlinNoise(desertPower, desertOctave, GenerateBaseNoise());
         waterPerlinNoise = GeneratePerlinNoise(waterPower, waterOctave, GenerateBaseNoise());
     }
 
@@ -38,62 +31,21 @@ public class PerlinNoise : MonoBehaviour
         // 6 = water
         // 7 = mountain
 
-        // preset 1
-        if (generationType == Type.PRESET_1)
-        {
-            if (desertPerlinNoise[x, y] < 0.16f)
-                ID = 4;
-            else if (heightPerlinNoise[x, y] > 0.1f)
-                ID = 3;
-            else if (heightPerlinNoise[x, y] > 0.075f)
-                ID = 5;
-            else
-                ID = 7;
-            if (waterPerlinNoise[x, y] < 0.1)
-                ID = 6;
-        }
+        // height-based biomes
+        if (heightPerlinNoise[x, y] < 0.16f)
+            ID = 4; // desert
+        else if (heightPerlinNoise[x, y] < 0.3f)
+            ID = 3; // grass
+        else if (heightPerlinNoise[x, y] < 0.4f)
+            ID = 5; // snow
 
-        // preset 2
-        if (generationType == Type.PRESET_2)
-        {
-            if (desertPerlinNoise[x, y] < 0.18f)
-                ID = 4;
-            else if (desertPerlinNoise[x, y] < 0.2f)
-                ID = 6;
-            else if (desertPerlinNoise[x, y] < 0.3f)
-                ID = 3;
-            else if (desertPerlinNoise[x, y] > 0.45f)
-                ID = 5;
-            else if (desertPerlinNoise[x, y] > 0.4f)
-                ID = 7;
-        }
+        // water noise is separate
+        if (waterPerlinNoise[x, y] < 0.1)
+            ID = 6;
 
-        // preset 3
-        if (generationType == Type.PRESET_3)
-        {
-            // bool mountainCondition1 = desertPerlinNoise[x, y] > 0.9975f || desertPerlinNoise[x, y] < 0.025f;
-            //bool mountainCondition2 = desertPerlinNoise[x, y] > 0.365f || desertPerlinNoise[x, y] < 0.375f;
-            if (heightPerlinNoise[x, y] < 0.16f)
-                ID = 4;
-            else if (heightPerlinNoise[x, y] < 0.3f)
-                ID = 3;
-            else if (heightPerlinNoise[x, y] < 0.4f)
-                ID = 5;
-            // else if (mountainCondition1||mountainCondition2)
-            //  ID = 7;
-
-            //else if (desertPerlinNoise[x, y] < 0.4f)
-               // ID = 7;
-
-
-            //if (desertPerlinNoise[x, y] < 0.1f)
-            //   ID = 7;
-            if (waterPerlinNoise[x, y] < 0.1)
-                ID = 6;
-
-            if (Random.Range(0, 100) < 5)
-                ID = 7;
-        }
+        // mountains are simply random roll to achieve more "scattered" effect
+        if (Random.Range(0, 100) < 5)
+            ID = 7;
 
         return ID;
     }
