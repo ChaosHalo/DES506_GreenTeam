07a64f7fc8406a578c3095d70cafb7cc4d8e0a3c
ChaosHalo <642450669@qq.com>
2023-07-19 15:34:02 +0100

    delete orange button

---
+++ Assets/Scripts/UI/BackToMenuUIManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class BackToMenuUIManager : MonoBehaviour
+{
+    public UnityEvent UnityEvent;
+    private void OnEnable()
+    {
+        UnityEvent?.Invoke();
+    }
+}
