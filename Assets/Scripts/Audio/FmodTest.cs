d8ad63da3dd929932dbf6269864e46d6c050c06a
ChaosHalo <642450669@qq.com>
2023-07-19 15:00:35 +0100

    fmod test

--- Assets/Scripts/Audio/FmodTest.cs
+++ Assets/Scripts/Audio/FmodTest.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 using FMODUnity;
 public class FmodTest : MonoBehaviour
 {
+    private FMOD.Studio.EventInstance eventInstance;
+    private FMOD.Studio.PARAMETER_ID parameterId;
+    private FMODUnity.StudioEventEmitter StudioEventEmitter;
+    public float RPM;
+    public float Load;
     //开始
     // Start is called before the first frame update
     void Start()
     {
-        
+        StudioEventEmitter = GetComponent<StudioEventEmitter>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*eventInstance.setParameterByName("RPM", RPM);
+        eventInstance.setParameterByName("Load", Load);*/
+        StudioEventEmitter.SetParameter("RPM", RPM);
     }
 }
