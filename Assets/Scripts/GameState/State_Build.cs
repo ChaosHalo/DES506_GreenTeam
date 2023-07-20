48622115a2433f07139aee98bdf1971ee697c0a6
SerOleg <58004206+SerOleg@users.noreply.github.com>
2023-07-21 00:58:48 +0100

    Disable world animation after race

--- Assets/Scripts/GameState/State_Build.cs
+++ Assets/Scripts/GameState/State_Build.cs
@@ -21,7 +21,7 @@ public class State_Build : IGameState
 
         //MyGameManager.instance.GetMissionManager().CheckForCompletedMissions();
         MyGameManager.instance.GetCameraManager().ResetCamera();
-        MyGameManager.instance.GetObjectPlacer().TriggerFaillAnimations();
+        //MyGameManager.instance.GetObjectPlacer().TriggerFaillAnimations();
         MyGameManager.instance.SetShadowQuality(0);
     }
     public void EndState()
