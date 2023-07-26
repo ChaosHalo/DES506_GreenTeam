a5f25ce5e02f2b42b3a93f6dd1d0b1aec074c33d
ChaosHalo <642450669@qq.com>
2023-07-26 13:54:42 +0100

    Season Finale Map Screenshot

---
+++ Assets/Scripts/Camera/CameraScreenshot.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.Networking;
+using System.Threading.Tasks;
+
+public class CameraScreenshot : MonoBehaviour
+{
+    public int width = 1920; // 截图的宽度
+    public int height = 1080; // 截图的高度
+
+    public Camera mainCamera;
+    public List<Texture2D> ScreenShots = new();
+    private void Start()
+    {
+        //mainCamera = Camera.main;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TakeScreenshot(1);
+        }
+    }
+    public void TakeScreenshot(int index)
+    {
+        mainCamera.GetComponent<Camera>().enabled = true;
+        // 创建一个RenderTexture来保存相机画面
+        RenderTexture renderTexture = new RenderTexture(width, height, 24);
+        mainCamera.targetTexture = renderTexture;
+        mainCamera.Render();
+
+        // 将RenderTexture转换为Texture2D
+        Texture2D screenshot = new Texture2D(width, height, TextureFormat.RG16, false);
+        RenderTexture.active = renderTexture;
+        screenshot.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        screenshot.Apply();
+
+        ScreenShots.Add(screenshot);
+    }
+    
+    IEnumerator GetText(string path)
+    {
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(path))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                // Get downloaded asset bundle
+                var texture = DownloadHandlerTexture.GetContent(uwr);
+                Debug.Log(texture.name);
+            }
+        }
+    }
+    private async void ImageTest()
+    {
+        await ImageTestAsync();
+        Debug.Log("Image loading complete!");
+    }
+
+    async Task ImageTestAsync()
+    {
+        var imageData = File.ReadAllBytes("Assets/Resources/SaveData/MapScreenShots/MapScreenshot1.jpg");
+        var texture = new Texture2D(1, 1);
+        var loaderSettings = AsyncImageLoader.LoaderSettings.Default;
+        var success = false;
+
+        // Load image data into existing texture
+        // Use the default LoaderSettings
+        success = await AsyncImageLoader.LoadImageAsync(texture, imageData);
+
+        // Similar to ImageConversion.LoadImage
+        // Mark texture as unreadable after reading.
+        success = await AsyncImageLoader.LoadImageAsync(texture, imageData, true);
+
+        // Use a custom LoaderSettings
+        success = await AsyncImageLoader.LoadImageAsync(texture, imageData, loaderSettings);
+
+        // Create new texture from image data
+        // Use the default LoaderSettings
+        texture = await AsyncImageLoader.CreateFromImageAsync(imageData);
+
+        // Use a custom LoaderSettings
+        texture = await AsyncImageLoader.CreateFromImageAsync(imageData, loaderSettings);
+
+        //Debug.Log(texture.name);
+    }
+}
