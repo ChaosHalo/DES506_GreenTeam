d54d1f3818a5f584eae367b2caa6ed2f2297d636
ChaosHalo <642450669@qq.com>
2023-08-16 15:00:08 +0100

    fmod adjustment

--- Assets/Scripts/Audio/FMODCarController.cs
+++ Assets/Scripts/Audio/FMODCarController.cs
@@ -71,7 +71,7 @@ public class FMODCarController : MonoBehaviour
     {
         Load = _Load;
         StudioEventEmitter.SetParameter("Load", _Load);
-        UnityEngine.Debug.Log("Load" + _Load);
+        UnityEngine.Debug.Log("Load" + Load);
     }
     public void TurnOffSound()
     {
